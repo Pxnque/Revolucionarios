c94cc4245b940b18f779839b570021299f7ddc80
Miguel <miguelhernandez353@outlook.com>
2024-05-05 23:55:31 -0600

    arreglamos el menu de cada platillo

--- src/app/components/platillo/Platillo.js
+++ src/app/components/platillo/Platillo.js
@@ -9,36 +9,30 @@
     return data?.items || [];
 
   }
-  
 
 export default async function DisplayPlatillos() {
     const platillos = await getPlatillos();
   
     return (
         <div>
-            <div className="flex items-center justify-center flex-wrap max-w-screen-xl">
-                {platillos?.map((cat) => {
-                    return <Platillo key={cat.nombre} note={cat}/>;
-                })}
-            </div>
+            {platillos?.map((platillo) => {
+                return <Platillo key={platillo.nombre} platillo={platillo} />;
+            })}
         </div>
     );
 }
 
-function Platillo({ note }) {
-    const { id, nombre, ingredientes, tiempoPrep } = note || {};
-    //const baseUrl = 'http://127.0.0.1:8090/api/files/categoria/';
-    //const imageUrl = imagen;
-    return (
-        
-            <div className="text-black">
-                
-                <h1>{nombre}</h1>
-                
-                <p>{ingredientes}</p>
-                <p className="font-extralight">{tiempoPrep}</p>
+function Platillo({ platillo }) {
+    const { nombre, ingredientes, tiempoPrep } = platillo || {};
 
-            </div>
-        
+    return (
+        <div className="container mx-auto px-4 md:mx-52">
+
+        <h1 className="text-xl text-red-800 font-bold ">{nombre}</h1>
+
+        <p className="text-base ">{ingredientes}</p>
+        <p className="font-extralight">{tiempoPrep}</p>
+
+    </div>
     );
 }
