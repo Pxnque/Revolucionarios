1b99efc8e01b1e6a642f55b21cf5c5a43d91ae7f
Pxnque <panque08@outlook.es>
2024-05-15 00:07:04 -0600

    el fetch API de comida funciona a la perfeccion

--- src/app/adminRegister/page.jsx
+++ src/app/adminRegister/page.jsx
@@ -5,10 +5,11 @@ import { fetchData } from '@/app/fetchData';
 
 
 const apiData = fetchData("http://127.0.0.1:8090/api/collections/categoria/records?fields=id,nombreCat,expand.relField.name");
-
+const apiDataComida = fetchData("http://127.0.0.1:8090/api/collections/comida/records?fields=id,nombre,ingredientes,tiempoPrep,expand.relField.name?page=1&perPage=200");
 
 const page = () => {
   const [data, setData] = useState(null);
+  const [dataComida, setDataComida] = useState(null);
 
     useEffect(() => {
         try {
@@ -21,12 +22,29 @@ const page = () => {
         } catch (error) {
             console.error('Error fetching data:', error);
         }
+        try {
+          const fetchedDataComida = apiDataComida.read();
+          if (fetchedDataComida && fetchedDataComida.items) {
+              setDataComida(fetchedDataComida.items);
+          } else {
+              console.error('Data is not in expected format:', fetchedDataComida);
+          }
+      } catch (error) {
+          console.error('Error fetching data:', error);
+      }
+        
     }, []);
 
-    const renderOptions = () => {
+    const renderOptionsCategorias = () => {
       return data?.map(record => (
           <option key={record.id} value={record.nombreCat}>{record.nombreCat}</option>
       ));
+  };
+
+  const renderOptionsComidas = () => {
+    return dataComida?.map(record => (
+      <option key={record.id} value={record.nombre}>{record.nombre}</option>
+    ));
   };
  
   return (
@@ -34,18 +52,18 @@ const page = () => {
           <div className="flex flex-col items-center justify-center my-4">
             <h1 className="font-bold text-3xl text-center mb-4 md:text-4xl">Editar Platillo</h1>
             <select className=" w-1/2 md:w-1/3 px-4 py-2 border border-black rounded-md text-xl text-center font-bold h-full">
-            <option value="" disabled selected>Selecciona una platillo</option>
+            {renderOptionsComidas()}
       </select>
       <textarea name="" id="" className="border border-black rounded-md m-4 w-1/2 md:w-1/3 px-4 py-2 text-xl "></textarea>
-      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Tiempo de espera en min"/>
-      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Precio"/>
+      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Tiempo de preparación en min"/>
+      
     <button className="border border-black rounded-md w-1/2 md:w-1/3 px-4 py-2 hover:text-white hover:bg-red-700 font-bold text-xl">Aceptar</button>
       </div>
      
       <div className="flex flex-col items-center justify-center my-4">
             <h1 className="font-bold text-3xl text-center mb-4 md:text-4xl w-full">Cambiar Imagen Categorias</h1>
-            <select className=" w-1/2 md:w-1/3 px-4 py-2 border border-black rounded-md text-xl text-center font-bold h-full mb-4">
-            {renderOptions()}
+            <select id="Categorias-option1" className=" w-1/2 md:w-1/3 px-4 py-2 border border-black rounded-md text-xl text-center font-bold h-full mb-4">
+            {renderOptionsCategorias()}
       </select>
       <div className="flex">
         <p className="font-semibold text-2xl px-4 py-2 mb-4">Imagen</p>
@@ -57,12 +75,12 @@ const page = () => {
       <div className="flex flex-col items-center justify-center my-4">
             <h1 className="font-bold text-3xl text-center mb-4 md:text-4xl">Agregar Platillo</h1>
             <input type="text" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Nombre"/>
-            <select className=" w-1/2 md:w-1/3 px-4 py-2 border border-black rounded-md text-xl text-center font-bold h-full">
-            <option value="" disabled selected>Selecciona una categoria</option>
+            <select id="Categorias-option2" className=" w-1/2 md:w-1/3 px-4 py-2 border border-black rounded-md text-xl text-center font-bold h-full">
+            {renderOptionsCategorias()}
       </select>
       <textarea name="" id="" className="border border-black rounded-md m-4 w-1/2 md:w-1/3 px-4 py-2 text-xl "></textarea>
-      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Tiempo de espera en min"/>
-      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Precio"/>
+      <input type="number" className="w-1/2 md:w-1/3 px-4 py-2 border border-black text-black rounded-sm mb-4" placeholder="Tiempo de preparació en min"/>
+     
     <button className="border border-black rounded-md w-1/2 md:w-1/3 px-4 py-2 hover:text-white hover:bg-red-700 font-bold text-xl">Aceptar</button>
       </div>
 
