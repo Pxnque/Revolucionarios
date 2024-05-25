4b3e61fda4ba269eb9cfc4de8caa7a361e70fd60
Pxnque <panque08@outlook.es>
2024-05-25 14:47:17 -0600

    Se refactorizo todas las llamadas a la API, ahora se utiliza las funciones nativas de pocketbase

--- src/app/components/NewCategoria/NewCategoria.js
+++ src/app/components/NewCategoria/NewCategoria.js
@@ -1,31 +1,26 @@
-
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Suspense } from 'react';
-import { fetchData } from '@/app/fetchData';
 import Link from 'next/link';
+import PocketBase from 'pocketbase';
 
-
-const url = 'https://revolucionarios.pockethost.io/'
-
-const apiData = fetchData("https://revolucionarios.pockethost.io/api/collections/categoria/records?page=1&perPage=30");//Lectura de la api
+const pb = new PocketBase('https://revolucionarios.pockethost.io');
 
 const NewCategoria = () => {
-    const [data, setData] = useState(null);//estado de la data, se inicializa en nulo
+    const [data, setData] = useState(null); // estado de la data, se inicializa en nulo
 
     useEffect(() => {
-        try {
-            const fetchedData = apiData.read();//se realiza el suspense y la promesa para obtener la informacion y se guarda en la variable
-            if (fetchedData && fetchedData.items) {
-                setData(fetchedData.items);
-            } else {
-                console.error('Data is not in expected format:', fetchedData);
+        const fetchData = async () => {
+            try {
+                const resultList = await pb.collection('categoria').getList(1, 50);
+                setData(resultList.items);
+            } catch (error) {
+                console.error('Error fetching data:', error);
             }
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+        };
+        fetchData();
     }, []);
 
-    const baseUrl = 'https://revolucionarios.pockethost.io/api/files/categoria/';//url base de la api para la recuperaciond de las imagenes
+    const baseUrl = 'https://revolucionarios.pockethost.io/api/files/categoria/'; // url base de la api para la recuperaciond de las imagenes
 
     return (
         <div className="flex items-center justify-center flex-wrap max-w-screen-xl">
--- src/app/menu/[id]/page.jsx
+++ src/app/menu/[id]/page.jsx
@@ -1,50 +1,48 @@
 "use client"
 import Footer from "@/app/components/footer/Footer";
 import Navbar from "@/app/components/navbar/Navbar";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Suspense } from 'react';
-import { fetchDataP } from "@/app/fetchDataP";
+import PocketBase from 'pocketbase';
 
-export default function PlatilloId ({ params }){
+const pb = new PocketBase('https://revolucionarios.pockethost.io');
+
+export default function PlatilloId({ params }) {
     const [data, setData] = useState(null);
     const idComida = params.id;
-    const url = `https://revolucionarios.pockethost.io/api/collections/comida/records?filter=idCategoria='${idComida}'`;
-    
-    useEffect(() => {//llama a la API para recuperar la informacion de los platillos
-        fetchDataP(url)
-            .then(data => {
-                if (data && data.items) {
-                    setData(data.items);  //Actualiza el estado del array de datos
-                } else {
-                    console.error('No items found in response');
-                }
-            })
-            .catch(error => {
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const resultList = await pb.collection('comida').getList(1, 50, {
+                    filter: `idCategoria='${idComida}'`
+                });
+                setData(resultList.items);
+            } catch (error) {
                 console.error('Error fetching data:', error);
-            });
-    }, [url]);
-    
-    return(
+            }
+        };
+        fetchData();
+    }, [idComida]);
+
+    return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="flex items-center justify-center flex-wrap max-w-screen-xl">
-            <Suspense fallback={<div>Loading...</div>}>
-                <ul className="flex items-center justify-center flex-wrap max-w-screen-xl">
-                    {data?.map((item) => ( //mapea los platillos recuperados de la llamada a la API
-                        <div key={item.id} className="container mx-auto px-4 md:mx-52 justify-center items-center">
-
-                            <h1 className="text-xl text-red-800 font-bold md:text-5xl">{item.nombre}</h1>
-
-                            <p className="text-base md:text-3xl font-semibold">{item.ingredientes}</p>
-                            <p className="md:text-2xl">{item.tiempoPrep}</p>
-                        <div className="border border-black border-dashed w-full"/>
-                                        
-                    </div>
-                    ))}
-                </ul>
-            </Suspense>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <ul className="flex items-center justify-center flex-wrap max-w-screen-xl">
+                        {data?.map((item) => (
+                            <div key={item.id} className="container mx-auto px-4 md:mx-52 justify-center items-center">
+                                <h1 className="text-xl text-red-800 font-bold md:text-5xl">{item.nombre}</h1>
+                                <p className="text-base md:text-3xl font-semibold">{item.ingredientes}</p>
+                                <p className="md:text-2xl">{item.tiempoPrep}</p>
+                                <div className="border border-black border-dashed w-full" />
+                            </div>
+                        ))}
+                    </ul>
+                </Suspense>
             </div>
-            <Footer/>
+            <Footer />
         </div>
-    ); 
+    );
 }
